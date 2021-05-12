6f452c5bd4c42fb40e30f1597f5fef7d26a2d98a
mishrabiswajeet <52379554+mishrabiswajeet@users.noreply.github.com>
2021-05-12 22:36:02 +0530

    Create Base 7.py

---
+++ Base 7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        rem=0
+        output=[]
+        check = False     #check = True num=7
+        if num < 0: #num = -7
+            check = True
+            num = num*(-1)  
+        elif num==0:
+            return str(num)
+        while num!=0:
+            rem=num%7
+            num=num//7
+            output.append(str(rem))r
+
+        output.reverse()
+        output=''.join(output)
+        if check:
+            return '-'+str(output)
+        return str(output)
