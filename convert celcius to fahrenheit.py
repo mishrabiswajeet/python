fdea34f334136f55c8f5c99fcc59c09d97dd6a5f
mishrabiswajeet <52379554+mishrabiswajeet@users.noreply.github.com>
2021-06-30 15:05:39 +0530

    Update convert celcius to fahrenheit.py

--- convert celcius to fahrenheit.py
+++ convert celcius to fahrenheit.py
@@ -2,3 +2,13 @@ celcius = int(input("Enter the celcius:"))
 fahrenheit = (celcius*(9/5)+32)
 fahren = "{:.1f}".format(fahrenheit)
 print(fahren)
+
+
+convert fahrenheit to celcius:
+  
+ f=int(input("Enter the fahrenheit:"))
+celcius = ((fahrenheit-32)*(5/9))
+print(celcius)
+
+
+convert 
