fd97b5e908ceef153ece900c07c20fb0232b8e90
mishrabiswajeet <52379554+mishrabiswajeet@users.noreply.github.com>
2021-06-30 21:06:13 +0530

    Update prime number.py

--- prime number.py
+++ prime number.py
@@ -9,5 +9,26 @@ if num>1:
         print(num,"is a prime number")
 else:
     print(num,"is not a prime number")
+    
+    
+  ## Prime number for the certian interval:
+
+
+lower = int(input("Enter lower range:"))
+upper = int(input("Enter upper range:"))
+for num in range(lower,upper+1):
+    if num>1:
+        for i in range(2,num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+
+    
+    
+   
+
+
+
 
 
