2c7752ac90b700e69fa7f9cd6ce6566a3d103023
mishrabiswajeet <52379554+mishrabiswajeet@users.noreply.github.com>
2021-06-30 10:14:45 +0530

    Create random number.py

---
+++ random number.py
@@ -0,0 +1,25 @@
+step-1:
+  
+import random
+random_list=[]
+for i in range(1,10):
+    n=random.randint(1,50)
+    random_list.append(n)
+print(random_list)
+
+
+step-2:This will generate the random number with the infinite range
+  
+import random
+n=random.random()
+print(n)
+
+
+
+step-3:This will generate the random number with the finite range
+  
+  
+import random
+n=random.randint(1,50)
+print(n)
+    
